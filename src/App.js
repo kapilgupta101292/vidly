19ffd7e545c574efb3cd7833f2d63ecbb6b40934
kapil.gupta <kapil.gupta@sirionlabs.com>
2020-06-01 11:10:29 +0530

    use of refrence to focus - in comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
 import MovieForm from "./components/movieForm";
 import Movie from "./components/movie";
 import Navbar from "./components/navbar";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
+import NotFound from "./components/common/notFound";
+import LoginForm from "./components/common/loginForm";
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/font-awesome/css/font-awesome.css";
-import NotFound from "./components/common/notFound";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Navbar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movie} />
           <Route path="/customers" component={Customers} />
